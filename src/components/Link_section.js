a81435f9072cd8d6d69ec33b40b404f546db9fcb
Oleksii Lobok <zhuyka1993@gmail.com>
2024-10-14 17:42:38 +0300

    edit path to images

--- src/components/Link_section.js
+++ src/components/Link_section.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import LinkItem from './LinkItem';
-import '../style/StyleForComponents.css';
+import React from "react";
+import LinkItem from "./LinkItem";
+import "../style/StyleForComponents.css";
 
 const LinkSection = () => {
   return (
-    <div className='links' id="services">
-      <LinkItem 
-        href='#gym' 
-        imgSrc='/gym.png' 
-        altText='Gym' 
-        text='ТРЕНАЖЕРНИЙ ЗАЛ' 
-        bgClass='gym_link' 
+    <div className="links" id="services">
+      <LinkItem
+        href="#gym"
+        imgSrc={`${process.env.PUBLIC_URL}/gym.png`}
+        altText="Gym"
+        text="ТРЕНАЖЕРНИЙ ЗАЛ"
+        bgClass="gym_link"
       />
-      <LinkItem 
-        href='#group' 
-        imgSrc='/group.png' 
-        altText='Group' 
-        text='ГРУПОВІ ЗАНЯТТЯ' 
-        bgClass='group_link' 
+      <LinkItem
+        href="#group"
+        imgSrc={`${process.env.PUBLIC_URL}/group.png`}
+        altText="Group"
+        text="ГРУПОВІ ЗАНЯТТЯ"
+        bgClass="group_link"
       />
-      <LinkItem 
-        href='#sol' 
-        imgSrc='/sun.png' 
-        altText='Solarium' 
-        text='СОЛЯРІЙ' 
-        bgClass='solari_link' 
+      <LinkItem
+        href="#sol"
+        imgSrc={`${process.env.PUBLIC_URL}/sun.png`}
+        altText="Solarium"
+        text="СОЛЯРІЙ"
+        bgClass="solari_link"
       />
-      <LinkItem 
-        href='#massage' 
-        imgSrc='/relax.png' 
-        altText='Massage' 
-        text='МАСАЖ' 
-        bgClass='massage_link' 
+      <LinkItem
+        href="#massage"
+        imgSrc={`${process.env.PUBLIC_URL}/relax.png`}
+        altText="Massage"
+        text="МАСАЖ"
+        bgClass="massage_link"
       />
-      <LinkItem 
-        href='#cafe' 
-        imgSrc='/cafe.png' 
-        altText='Cafe' 
-        text='КАФЕ' 
-        bgClass='cafe_link' 
+      <LinkItem
+        href="#cafe"
+        imgSrc={`${process.env.PUBLIC_URL}/cafe.png`}
+        altText="Cafe"
+        text="КАФЕ"
+        bgClass="cafe_link"
       />
     </div>
   );
-}
+};
 
 export default LinkSection;
